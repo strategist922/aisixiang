a676d8c72906440569f0624e29dcba399619ae0c
cc <cc@ccdeMacBook-Air.local>
2017-05-26 03:42:54 +0800

    update

--- 02.download.py
+++ 02.download.py
@@ -7,7 +7,7 @@ import pandas
 import time
 
 # 读取 00.get_metadata.R 获取的相关目录信息
-D0 = pandas.read_csv("all_aisixiang_2017-05-18_copy.csv")
+D0 = pandas.read_csv("all_aisixiang_2017-05-24.csv")
 
 # 意外中断时，可以修改 j 的值
 j = 0
@@ -20,7 +20,7 @@ for i in D['ID']:
         html = urlopen(Url)
     except:
         f1 = open("broken-new.txt", 'a')
-        Broken = str(j) + '-' + Url + ',' + '\n'
+        Broken = str(i) + '-' + Url + ',' + '\n'
         f1.write(Broken)
         f1.close
         print(Broken)
@@ -29,7 +29,7 @@ for i in D['ID']:
         Availability = 3
 
         f2 = open("Av.txt", 'a')
-        f2.write(str(Availability) + '_' + str(j) + ',' + '\n')
+        f2.write(str(Availability) + '_' + str(i) + ',' + '\n')
         f2.close
 
     else:
@@ -49,15 +49,15 @@ for i in D['ID']:
             else:
                 Page_number = Page.find_all("a")
                 N = int(Page_number[-2].get_text())
-                for i in range(1, N+1):
-                    Url2 = Url[:(len(Url)-5)] + '-' + str(i) + '.html'
+                for k in range(1, N+1):
+                    Url2 = Url[:(len(Url)-5)] + '-' + str(k) + '.html'
                     print(Url2)
                     try:
                         html2 = urlopen(Url2)
                     except:
-                        i += 1
+                        k += 1
                         ft2 = open("broken2.txt", 'a')
-                        Broken2 = str(j + 1) + '-' + Url2 + ',' + '\n'
+                        Broken2 = str(i) + '-' + Url2 + ',' + '\n'
                         ft2.write(Broken2)
                         ft2.close
                         print(Broken2)
@@ -66,7 +66,7 @@ for i in D['ID']:
                         Article = Soup2.find(id = "content2")
                         Article_page = Article_page + Article.get_text()
                         time.sleep(1)
-        Name = str(Availability) + '-' + str(j+1) + '-' + D0.iloc[j,0] + '.txt'
+        Name = str(Availability) + '-' + str(i) + '-' + D0.iloc[j,0] + '.txt'
         Name = Name.replace('/','')
         f = open(Name, 'w')
         f.write(Article_page)
@@ -76,5 +76,5 @@ for i in D['ID']:
         time.sleep(1)
 
         f2 = open("Av.txt", 'a')
-        f2.write(str(Availability) + '_' + str(j) + ',' + '\n')
+        f2.write(str(Availability) + '_' + str(i) + ',' + '\n')
         f2.close
